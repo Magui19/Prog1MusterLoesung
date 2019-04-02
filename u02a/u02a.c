cf2aa6c949b4cff9cb361a551713134fa1b145f1
LADBukkit <LADBukkit@gmail.com>
2019-04-02 16:28:49 +0200

    u02a

--- u02a/u02a.c
+++ u02a/u02a.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
 int main() {
+	/* Deklarieren der Variablen */
+
+	/* Zaehler fuer die Monate */
 	int monat;
-	int anzTage;
+
+	/* Zaehler fuer die Tage */
 	int tag;
+
+	/* Platzhalter fuer die Anzahl der Tage im Monat */
+	int anzTage;
+
+	/* Platzhalter fuer den letzten Wochentag des vorhergegangenen Monats */
 	int wochentag = 0;
 
+	/* Alle Monate durchlaufen */
 	for(monat = 1; monat <= 12; monat++) {
 		printf("\n");
+
+		/* switch case um die Anzahl der Tage zu ermitteln */
 		switch(monat) {
 		case 1:
 			anzTage = 31;
+			/* Ausgabe des Monatsnamen (Aufgabe c.) */
 			printf("Januar\n");
 			break;
 		case 2:
@@ -59,17 +72,27 @@ int main() {
 			break;
 		}
 
+		/* Einruecken anhand des Wochentages, */
+		/* mit dem der letzte Monat aufgehoert hat */
 		for(int i = 0; i < wochentag; i++) {
 			printf("\t");
 		}
 
+		/* Alle Tage im Monat durchlaufen */
 		for(tag = 1; tag <= anzTage; tag++) {
 			printf("%i\t", tag);
+
+			/* Wenn ein Sonntag erreicht ist -> Linebreak */
+			/* wochentag ist hierbei der Offset, da wir das ganze */
+			/* um den letzten Tag des vorherigen Monats verschieben wollen */
 			if((tag + wochentag) % 7 == 0) {
 				printf("\n");
 			}
 		}
+
+		/* Errechnen des Wochentages des letzten Monats */
 		wochentag = wochentag + (anzTage % 7);
+		/* Ueberlaufe verhindern */
 		wochentag = wochentag % 7;
 
 		printf("\n");
